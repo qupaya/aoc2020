fb7204645890f8ac254e37f170553ceecb3a51ef
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-01 08:46:19 +0100

    feat(markus-api): add solution for day 1

---
+++ apps/markus/api/src/day1/day1.go
@@ -0,0 +1,38 @@
+package day1
+
+import "fmt"
+
+// SolveTask1 of day 1
+func SolveTask1(input []int) int {
+	for i, a := range input {
+        for j, b := range input {
+			if i != j && a + b == 2020 {
+				return a * b
+			}
+		}
+    }
+
+	return -1
+}
+
+// SolveTask2 of day 1
+func SolveTask2(input []int) int {
+	for i, a := range input {
+        for j, b := range input {
+			for k, c := range input {
+				if i != j && i != k && j != k && a + b + c == 2020 {
+					return a * b * c
+				}
+			}
+		}
+    }
+
+	return -1
+}
+
+// Run day1
+func Run() {
+	input := Input()
+	fmt.Println("solution task 1:", SolveTask1(input))
+	fmt.Println("solution task 2:", SolveTask2(input))
+}
