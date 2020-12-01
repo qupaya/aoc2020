a586fe252bfb352ff53261ef50613776a72c36e0
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-01 10:46:29 +0100

    fix(markus-api): fix build error

--- apps/markus/api/src/main.go
+++ apps/markus/api/src/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"aoc/apps/markus/api/src/day1"
 	"fmt"
 )
 
 func main() {
-	input := day1.Input()
-	fmt.Println("day1, task 1:", day1.SolveTask1(input))
-	fmt.Println("day1, task 2:", day1.SolveTask2(input))
+	fmt.Println("hello world")
 }
