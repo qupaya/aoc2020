2cb75d8efdbbb7130b166adb7e9ac90a316ff796
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-03 20:42:06 +0100

    refactor(markus-api): improve day 3 solution

--- apps/markus/api/src/day3/day3.go
+++ apps/markus/api/src/day3/day3.go
@@ -6,15 +6,17 @@ import (
 
 func countTrees(input *[]string, down int, right int) int {
 	width := len((*input)[0])
-	countTrees := 0
+	height := len(*input)
 	x := 0
-	for y, row := range *input {
-		if y%down == 0 {
-			if row[x%width] == 35 {
-				countTrees++
-			}
-			x += right
+	y := 0
+	countTrees := 0
+
+	for y < height {
+		if (*input)[y][x%width] == 35 {
+			countTrees++
 		}
+		x += right
+		y += down
 	}
 	return countTrees
 }
@@ -26,7 +28,19 @@ func SolveTask1(input *[]string) int {
 
 // SolveTask2 of day 3
 func SolveTask2(input *[]string) int {
-	return countTrees(input, 1, 1) * countTrees(input, 1, 3) * countTrees(input, 1, 5) * countTrees(input, 1, 7) * countTrees(input, 2, 1)
+	slopes := [][]int{
+		{1, 1},
+		{1, 3},
+		{1, 5},
+		{1, 7},
+		{2, 1},
+	}
+
+	trees := 1
+	for _, s := range slopes {
+		trees *= countTrees(input, s[0], s[1])
+	}
+	return trees
 }
 
 // Run day3
