99f10c518e72f9b940ac339626de77ae4a9f2616
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-05 08:50:36 +0100

    feat(markus-api): run all solutions in main

--- apps/markus/api/src/main.go
+++ apps/markus/api/src/main.go
@@ -1,11 +1,23 @@
 package main
 
-import "aoc/apps/markus/api/src/day5"
+import (
+	"aoc/apps/markus/api/src/day1"
+	"aoc/apps/markus/api/src/day2"
+	"aoc/apps/markus/api/src/day3"
+	"aoc/apps/markus/api/src/day4"
+	"aoc/apps/markus/api/src/day5"
+	"fmt"
+)
 
 func main() {
-	//day1.Run()
-	//day2.Run()
-	//day3.Run()
-	// day4.Run()
+	fmt.Println("day 1")
+	day1.Run()
+	fmt.Println("day 2")
+	day2.Run()
+	fmt.Println("day 3")
+	day3.Run()
+	fmt.Println("day 4")
+	day4.Run()
+	fmt.Println("day 5")
 	day5.Run()
 }
