1c0b3b869cabe5027a5c48ff5548e979a6ed5287
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-05 07:06:23 +0100

    feat(markus-api): add solution for day 5

--- apps/markus/api/src/main.go
+++ apps/markus/api/src/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "aoc/apps/markus/api/src/day4"
+import "aoc/apps/markus/api/src/day5"
 
 func main() {
 	//day1.Run()
 	//day2.Run()
 	//day3.Run()
-	day4.Run()
+	// day4.Run()
+	day5.Run()
 }
