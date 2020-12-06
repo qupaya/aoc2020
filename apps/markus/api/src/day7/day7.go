53554255008820bbd36fd3219133a6d500663d98
Markus Ende <19502754+Markus-Ende@users.noreply.github.com>
2020-12-06 09:12:30 +0100

    feat(markus-api): prepare day 7

---
+++ apps/markus/api/src/day7/day7.go
@@ -0,0 +1,24 @@
+package day7
+
+import (
+	"fmt"
+)
+
+// SolveTask1 solve aoc task 1
+func SolveTask1(input *[]string) int {
+	return -1
+
+}
+
+// SolveTask2 solve aoc task 1
+func SolveTask2(input *[]string) int {
+	return -1
+}
+
+// Run solve and print solutions
+func Run() {
+	input := Input()
+
+	fmt.Println("solution task 1:", SolveTask1(&input))
+	fmt.Println("solution task 2:", SolveTask2(&input))
+}
---
+++ apps/markus/api/src/day7/input.go
@@ -0,0 +1,8 @@
+package day7
+
+import "strings"
+
+// Input aoc input
+func Input() []string {
+	return strings.Fields(``)
+}
